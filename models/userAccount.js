7eefdcbf4cd9a7bcfab4643ae48fc6cdf21574c0
s3977794 <s3977794@rmit.edu.vn>
2024-05-30 01:26:20 +0700

    `feat: Add userAccount model with schema and methods`

---
+++ models/userAccount.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String, 
+    required: true, 
+  }, 
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: Boolean,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    immutable: true,
+  },
+});
+
+userSchema.methods.getFormattedDateTime = function () {
+    const dateOptions = { day: "2-digit", month: "2-digit", year: "numeric" };
+
+    if (this.createdAt) {
+      const formattedDate = this.createdAt.toLocaleDateString(
+        "en-GB",
+        dateOptions
+      );
+      
+      return `${formattedDate}`;
+    } else {
+      return "";
+    }
+  };
+
+const UserModel = mongoose.model("Tài khoản", userSchema);
+
+
+module.exports = UserModel;
