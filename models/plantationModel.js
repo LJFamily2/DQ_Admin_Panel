0dc44d97e1c69248c572a2c870c4ac07373108a4
s3977794 <s3977794@rmit.edu.vn>
2024-06-06 22:38:24 +0700

    `chore: Update plantationModel.js for improved code structure and data organization`

--- models/plantationModel.js
+++ models/plantationModel.js
@@ -1,14 +1,57 @@
 const mongoose = require("mongoose");
 
 const plantationSchema = new mongoose.Schema({
-    name: {
+  name: {
+    type: String,
+    required: true,
+  },
+  areaID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Khu Vực",
+  },
+  managerID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Người Quản Lý",
+  },
+  code: {
+    type: String,
+    unique: true,
+  },
+  contactDuration: {
+    type: String,
+  },
+  plantationArea: {
+    type: String,
+  },
+  data: [
+    {
+      date: {
+        type: Date,
+      },
+      notes: {
         type: String,
-        required: true,
+      },
+      products: [
+        {
+          product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Hàng hóa",
+          },
+          quantity: {
+            type: Number,
+          },
+          percentage: {
+            type: Number,
+          },
+          total: {
+            type: Number,
+          },
+        },
+      ],
     },
+  ],
 });
 
-
 const plantationModel = mongoose.model("Vườn", plantationSchema);
-
 
 module.exports = plantationModel;
