fae852a993e070b2ac9f80971d9f03048b98d443
s3977794 <s3977794@rmit.edu.vn>
2024-06-21 01:19:15 +0700

    `Refactor routes/index.js to add new route for "/du-lieu"`

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,7 @@ const routes = [
     {path: "/quan-ly-khu-vuc", route: areaRoute},
     {path: "/quan-ly-vuon", route: plantationRoute},
     {path: "/truy-van", route: queryRoute},
+    {path: "/du-lieu", route: queryRoute},
 ]
 
 module.exports = routes;
