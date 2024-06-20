fcd7213995f54f8a776351dbf909476273be481c
s3977794 <s3977794@rmit.edu.vn>
2024-06-21 02:00:53 +0700

    Refactor dataModel.js to remove dryRubber and mixedRubber properties and add sellQuantity and sellPercentage properties

--- models/dataModel.js
+++ models/dataModel.js
@@ -4,13 +4,12 @@ const dataSchema = new mongoose.Schema({
     date: Date,
     notes: String,
     products: {
-        dryRubber: String,
         dryQuantity: Number,
         dryPercentage: Number,
-        mixedRubber: String,
         mixedQuantity: Number,
+        sellQuantity: Number,
+        sellPercentage: Number,
     },
-    sell: String,
 });
 
 const dataModel = mongoose.model('Dữ liệu', dataSchema);
