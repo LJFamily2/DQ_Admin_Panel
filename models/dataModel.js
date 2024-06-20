bfb181fc5e3951157e736b1d7036c80ba15300df
s3977794 <s3977794@rmit.edu.vn>
2024-06-21 01:11:59 +0700

    `Refactor dataModel.js to add schema for storing data related to a garden`

---
+++ models/dataModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+    date: Date,
+    notes: String,
+    products: {
+        dryRubber: String,
+        dryQuantity: Number,
+        dryPercentage: Number,
+        mixedRubber: String,
+        mixedQuantity: Number,
+    },
+    sell: String,
+});
+
+const dataModel = mongoose.model('Vườn', dataSchema);
+
+module.exports = dataModel;
