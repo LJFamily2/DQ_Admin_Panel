af0bf91247a28e7649210d8fe1c005cfedb4974f
s3977794 <s3977794@rmit.edu.vn>
2024-06-22 21:16:17 +0700

    `Refactor productTotalModel.js to add productTotalSchema for storing product total information`

---
+++ models/productTotalModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const productTotalSchema = new mongoose.Schema({
+    dryRubber: {
+        type: Number,
+        default: 0
+    },
+    mixedQuantity:{
+        type: Number,
+        default: 0
+    },
+    product:{
+        type:Number,
+        default: 0
+    }
+});
+
+const productTotalModel = mongoose.model('Dữ liệu Tổng', productTotalSchema);
+
+module.exports = productTotalModel;
+ 
