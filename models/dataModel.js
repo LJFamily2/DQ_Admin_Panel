2e9ec9963010928eb1583bd7a5fb3da961abb48a
s3977794 <s3977794@rmit.edu.vn>
2024-06-22 14:00:45 +0700

    `Refactor dataModel.js to add rawMaterialSchema for storing raw material information`

--- models/dataModel.js
+++ models/dataModel.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 
+const rawMaterialSchema = new mongoose.Schema({
+    name: String,
+    unit: String,
+    quantity: Number,
+    percentage: Number,
+});
+
 const dataSchema = new mongoose.Schema({
     date: Date,
     notes: String,
-    products: {
-        dryQuantity: Number,
-        dryPercentage: Number,
-        mixedQuantity: Number,
-    },
+    rawMaterial: [rawMaterialSchema]
 });
 
 const dataModel = mongoose.model('Dữ liệu', dataSchema);
