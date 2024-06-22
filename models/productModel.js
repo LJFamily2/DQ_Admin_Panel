ebb54377867959f157f0ead4ba0c02058d2ea837
s3977794 <s3977794@rmit.edu.vn>
2024-06-22 08:37:16 +0700

    `Refactor productModel.js to add dryRubberUsed and notes properties`

--- models/productModel.js
+++ models/productModel.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    name:String,
-    unit: String,
+    date: Date, 
     quantity: Number,
+    dryRubberUsed: Number,
+    notes: String,
 });
 
 
-const productModel = mongoose.model("Hàng hóa", productSchema);
+const productModel = mongoose.model("Thành phẩm", productSchema);
 
 
 module.exports = productModel;
