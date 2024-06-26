1f61b76f60d586ddb7fa2f218625be6ef7ea9292
s3977794 <s3977794@rmit.edu.vn>
2024-06-27 01:47:05 +0700

    `Refactor signInRoute.js to ensure user is logged out before accessing the login page`

--- routes/src/signInRoute.js
+++ routes/src/signInRoute.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const passport = require("passport");
 const connectEnsureLogin =  require('connect-ensure-login');
 const router = express.Router();
 const signInController = require('../../controllers/signInController')
 
-router.get("/", signInController.renderLogin)
-router.post('/login', signInController.handleLogin)
+router.get("/", connectEnsureLogin.ensureLoggedOut({redirectTo: '/ho-so'}), signInController.renderLogin)
+router.post('/login',connectEnsureLogin.ensureLoggedOut({redirectTo: '/ho-so'}), signInController.handleLogin)
 
 module.exports = router;
