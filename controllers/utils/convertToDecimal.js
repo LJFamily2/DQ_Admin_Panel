243a1388785ce32c4735f6d72f0d9692e335e986
s3977794 <s3977794@rmit.edu.vn>
2024-07-06 02:27:54 +0700

    Refactor convertToDecimal.js to handle input validation

--- controllers/utils/convertToDecimal.js
+++ controllers/utils/convertToDecimal.js
@@ -1,9 +1,11 @@
 function convertToDecimal(input) {
+  if (input) {
     // First, remove all periods assuming they are used as thousand separators
     let result = input.replace(/\./g, '');
     // Then, replace the first comma with a period to handle decimal separator
     result = result.replace(',', '.');
     return result;
   }
+}
 
 module.exports = convertToDecimal;
