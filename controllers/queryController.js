82b5cbf0f1d057b1ee09c9b030ceb66a7a054fd1
s3977794 <s3977794@rmit.edu.vn>
2024-08-03 00:42:14 +0700

    `Refactor code to update variable calculations in queryController.js`

--- controllers/queryController.js
+++ controllers/queryController.js
@@ -203,10 +203,10 @@ async function getDataTotal(req, res) {
 
       const dryPriceValue = convertToDecimal(dryPrice) || 0;
       const mixedPriceValue = convertToDecimal(mixedPrice) || 0;
-      const dryTotalValue = dryQuantityTotal * dryPriceValue || "";
-      const kePriceValue = convertToDecimal(dryPrice) - 4000 || '';
-      const keTotalValue = keQuantityTotal * kePriceValue || "";
-      const mixedTotalValue = mixedQuantityTotal * mixedPriceValue || "";
+      const dryTotalValue = dryQuantityTotal * dryPriceValue || 0;
+      const kePriceValue = convertToDecimal(dryPrice) - 4000 || 0;
+      const keTotalValue = keQuantityTotal * kePriceValue || 0;
+      const mixedTotalValue = mixedQuantityTotal * mixedPriceValue || 0;
       const totalMoneyValue = dryTotalValue + mixedTotalValue + keTotalValue;
 
       return {
