c93803e3ac2b3c6610e8a8c947ca6a1de2c3895e
s3977794 <s3977794@rmit.edu.vn>
2024-10-08 21:48:02 +0700

    feat: Add error handling for creating suppliers in createSuppliers function

--- controllers/dailySupplyController/helper/createSuppliers.js
+++ controllers/dailySupplyController/helper/createSuppliers.js
@@ -9,15 +9,20 @@ async function createSuppliers(req) {
   let supplierAddress = ensureArray(req.body.address);
   let supplierRatioSplit = ensureArray(req.body.ratioSplit);
 
-  const suppliers = supplierNames.map((name, index) => ({
-    name: name,
-    code: supplierCode[index],
-    phone: supplierPhone[index],
-    identification: supplierIdentification[index],
-    issueDate: supplierIssueDate[index],
-    address: supplierAddress[index],
-    ratioSplit: supplierRatioSplit[index],
-  }));
+  const suppliers = supplierNames.map((name, index) => {
+    // Only create supplier if the name is provided
+    if (name) {
+      return {
+        name: name,
+        code: supplierCode[index],
+        phone: supplierPhone[index],
+        identification: supplierIdentification[index],
+        issueDate: supplierIssueDate[index],
+        address: supplierAddress[index],
+        ratioSplit: supplierRatioSplit[index],
+      };
+    }
+  }).filter(Boolean); // Remove undefined entries
 
   return suppliers;
 }
