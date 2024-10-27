1a07d74f4e7371dc568aeda7bc496fced50195bc
s3977794 <s3977794@rmit.edu.vn>
2024-10-28 00:05:10 +0700

    Refactor actionHistoryController.js and actionHistoryPage.ejs: Add filtering functionality to action history page

--- controllers/actionHistoryController.js
+++ controllers/actionHistoryController.js
@@ -7,14 +7,62 @@ module.exports = {
 
 async function renderPage(req, res) {
   try {
-    const activities = await ActionHistory.find({}).populate({path: 'userId', select: 'username'});
-    
+    const {
+      startDate,
+      endDate,
+      selectedUser,
+      createAction,
+      updateAction,
+      deleteAction,
+    } = req.query;
+
+    // Build the query object
+    const query = {};
+    if (startDate) query.date = { $gte: new Date(startDate) };
+    if (endDate) {
+      if (!query.date) query.date = {};
+      query.date.$lte = new Date(endDate);
+    }
+    if (selectedUser) query.userId = selectedUser;
+
+    // Filter by action types
+    const actionTypes = [];
+    if (createAction) actionTypes.push('create');
+    if (updateAction) actionTypes.push('update');
+    if (deleteAction) actionTypes.push('delete');
+    if (actionTypes.length > 0) query.actionType = { $in: actionTypes };
+
+    const activities = await ActionHistory.find(query).populate({
+      path: 'userId',
+      select: ['username', 'role'],
+    });
+
+    // Extract unique users from activities
+    const usersMap = new Map();
+    activities.forEach(activity => {
+      const user = activity.userId;
+      if (user && !usersMap.has(user._id.toString())) {
+        usersMap.set(user._id.toString(), user.username);
+      }
+    });
+    const users = Array.from(usersMap, ([id, username]) => ({
+      _id: id,
+      username,
+    }));
+
     res.render('src/actionHistoryPage', {
       layout: './layouts/defaultLayout',
       title: 'Lịch sử hoạt động',
       activities,
       user: req.user,
+      users,
       messages: req.flash(),
+      startDate,
+      endDate,
+      selectedUser,
+      createAction,
+      updateAction,
+      deleteAction,
     });
   } catch (err) {
     console.log(err);
