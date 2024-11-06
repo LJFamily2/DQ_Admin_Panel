f939f45c5b6a451175db2da395058dc200489bdf
s3977794 <s3977794@rmit.edu.vn>
2024-11-07 00:56:02 +0700

    Refactor account routes: Consolidate middleware usage into commonMiddlewares for improved readability and maintainability

--- routes/src/accountRoute.js
+++ routes/src/accountRoute.js
@@ -3,56 +3,35 @@ const router = express.Router();
 const accountController = require('../../controllers/accountController');
 const authMiddlewares = require('../../middlewares/authMiddlewares');
 
+const commonMiddlewares = [
+  authMiddlewares.ensureLoggedIn,
+  authMiddlewares.ensureWorkingHours,
+  authMiddlewares.ensureAdmin
+];
+
 // Initial setup route
 router.get('/tao-tai-khoan', accountController.initialSetupPage);
-
 router.post('/tao-tai-khoan', accountController.initialSetupCreateAccount);
 
-router.get(
-  '/',
-  authMiddlewares.ensureLoggedIn,authMiddlewares.ensureWorkingHours,authMiddlewares.ensureAdmin,
-  accountController.renderPage,
-);
+// Main page
+router.get('/', ...commonMiddlewares, accountController.renderPage);
 
 // Get all users
-router.post(
-  '/getUsers',
-  authMiddlewares.ensureLoggedIn,authMiddlewares.ensureWorkingHours,authMiddlewares.ensureAdmin,
-  accountController.getUsers,
-);
+router.post('/getUsers', ...commonMiddlewares, accountController.getUsers);
 
 // Create a new user
-router.post(
-  '/createUser',
-  authMiddlewares.ensureLoggedIn,authMiddlewares.ensureWorkingHours,authMiddlewares.ensureAdmin,
-  accountController.createUser,
-);
+router.post('/createUser', ...commonMiddlewares, accountController.createUser);
 
 // Update a user by ID
-router.post(
-  '/update/:id',
-  accountController.updateUser,
-);
+router.post('/update/:id', accountController.updateUser);
 
 // Delete a user by ID
-router.post(
-  '/delete/:id',
-  authMiddlewares.ensureLoggedIn,authMiddlewares.ensureWorkingHours,authMiddlewares.ensureAdmin,
-  accountController.deleteUser,
-);
+router.post('/delete/:id', ...commonMiddlewares, accountController.deleteUser);
 
 // Delete all users
-router.post(
-  '/deleteAll',
-  authMiddlewares.ensureLoggedIn,authMiddlewares.ensureWorkingHours,authMiddlewares.ensureAdmin,
-  accountController.deleteAllUsers,
-);
+router.post('/deleteAll', ...commonMiddlewares, accountController.deleteAllUsers);
 
 // Log out
-router.post(
-  '/logOut',
-  authMiddlewares.ensureLoggedIn,authMiddlewares.ensureWorkingHours,
-  accountController.logOut,
-);
+router.post('/logOut', authMiddlewares.ensureLoggedIn, authMiddlewares.ensureWorkingHours, accountController.logOut);
 
 module.exports = router;
