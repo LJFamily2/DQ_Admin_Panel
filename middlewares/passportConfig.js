9347ce2d5f7334e453867aac00f106a5b6e92480
s3977794 <s3977794@rmit.edu.vn>
2024-11-19 16:15:48 +0700

    Update dependencies and enhance security: add helmet and express-rate-limit middleware

--- middlewares/passportConfig.js
+++ middlewares/passportConfig.js
@@ -3,7 +3,7 @@ const LocalStrategy = require("passport-local").Strategy;
 const UserModel = require("../models/accountModel");
 
 async function initialize(passport) {
-  const authenticater = async (username, password, done) => {
+  const authenticateUser = async (username, password, done) => {
     try {
       const user = await UserModel.findOne({ username: username });
       if (!user) {
@@ -21,9 +21,8 @@ async function initialize(passport) {
     }
   };
 
-  passport.use(new LocalStrategy({ usernameField: "username" }, authenticater));
+  passport.use(new LocalStrategy({ usernameField: "username" }, authenticateUser));
 
-  // serialize and deserialize are used to store the user in the session
   passport.serializeUser((user, done) => done(null, user.id));
   passport.deserializeUser(async (id, done) => {
     try {
