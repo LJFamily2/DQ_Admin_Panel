1b3c11e5cedfb3dead6fe3579c64561782af521d
s3977794 <s3977794@rmit.edu.vn>
2025-02-18 13:18:17 +0700

    Enhance footer callback in data table to log column headers and data before setup

--- public/js/datatable.js
+++ public/js/datatable.js
@@ -112,7 +112,12 @@ function initializeDataTable(
     const columns = [5, 6, 7, 8];
     footerCallbackOptions = {
       footerCallback: function () {
-        setupFooterCallback(columns, this.api());
+        const api = this.api();
+        columns.forEach(col => {
+          console.log(`Column ${col} header:`, $(api.column(col).header()).text());
+          console.log(`Column ${col} data:`, api.column(col, {search:'applied'}).data());
+        });
+        setupFooterCallback(columns, api);
       },
     };
   }
